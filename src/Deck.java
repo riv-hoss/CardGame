d3e27c32a8071c31730191a4de5494d791e332e5
Amin Hosseini <amin.hs117@gmail.com>
2020-08-13 20:51:31 +0200

    new commit

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Deck {
     private Card[] cards;
 
@@ -28,4 +30,148 @@ public class Deck {
     public Card[] getCards () {
         return this.cards;
     }
+
+    public void setCards(Card[] cards) {
+        this.cards = cards;
+    }
+
+    public void setCards(Card card, int i) {
+        this.cards[i] = card;
+    }
+
+    // shuffle deck
+    public void shuffle() {
+        for (int i = 0; i < cards.length ; i++) {
+            int index01 = randomInt(i, cards.length -1);
+            swapCards(i, index01);
+
+        }
+    }
+
+    // method within shuffle() - generate random number in a range
+    private static int randomInt (int low, int high) { // helper method - private
+        Random rnd = new Random();                      // instance method
+        int range = high - low +1;
+        return rnd.nextInt(range) + low ;
+    }
+
+    // method within shuffle() and selectionSort() - swap cards at two positions
+    private void swapCards (int i, int j) { // non-static since referred to this.cards
+        Card first = this.cards[i];         // helper method - private
+        cards[i] = cards[j];                // class method
+        cards[j] = first;
+    }
+
+
+    // sort deck
+    public void selectionSort () {
+        for (int i = 0; i < cards.length ; i++) {
+            int index01 = indexLowest(i, cards.length);
+            swapCards(i, index01);
+
+        }
+
+    }
+
+    // method within selectionSort() - find lowest card at or to the right of index i
+    public int indexLowest (int low, int high) {
+        Card minCard = cards[low];
+        int indexOfMin = low;
+        for (int j = low+1 ; j < high; j++) {
+            if (minCard.compareTo(cards[j]) > 0) {
+                minCard = cards[j];
+                indexOfMin = j;
+            }
+        }
+        return indexOfMin;
+    }
+
+
+    // generate sub deck
+    public Deck subDeck(int low, int high) {
+        Deck sub = new Deck(high - low + 1);
+        for (int i = 0; i < sub.cards.length; i++) {
+            sub.cards[i] = this.cards[low + i];
+        }
+        return sub;
+    }
+
+
+    // merge
+    private static Deck merge(Deck d1, Deck d2) {
+        // create a new deck, d3, big enough for all the cards
+        int d1Length = d1.getCards().length;
+        int d2Length = d2.getCards().length;
+        int totalCards = d1Length + d2Length;
+        Deck d3 = new Deck(totalCards);
+        // use the index i to keep track of where we are at in
+        // the first deck, and the index j for the second deck
+
+        int i = 0;
+        int j = 0;
+        // the index k traverses the result deck
+        for (int k = 0; k < totalCards; k++) {
+            // if d1 is empty, use top card from d2
+            // if d2 is empty, use top card from d1
+            // otherwise, compare the top two cards
+            // add lowest card to the new deck at k
+            // increment i or j (depending on card)
+            if (i >= d1Length) {
+                Card nextCard = d2.getCards()[j];
+                d3.setCards(nextCard, k);
+                j++;
+            } else if (j >= d2Length) {
+                Card nextCard = d1.getCards()[i];
+                d3.setCards(nextCard, k);
+                i++;
+            } else {
+                if (d1.getCards()[i].compareTo(d2.getCards()[j]) < 0) {
+                    Card nextCard = d1.getCards()[i];
+                    d3.setCards(nextCard, k);
+                    i++;
+                } else {
+                    Card nextCard = d2.getCards()[j];
+                    d3.setCards(nextCard, k);
+                    j++;
+                }
+            }
+        }
+        return d3; // return the new deck
+    }
+
+    // almost efficient
+    public Deck almostMergeSort() {
+
+        // divide the deck into two subdecks
+        Deck sub1 = this.subDeck(0, cards.length/2);
+        Deck sub2 = this.subDeck(cards.length/2 + 1, cards.length -1);
+        // sort the subdecks using selectionSort
+        sub1.selectionSort();
+        sub2.selectionSort();
+        // merge the subdecks, return the result
+
+
+        return merge(sub1, sub2);
+    }
+
+
+
+
+    // with recursive - efficient
+    public Deck mergeSort() {
+        int thisDeckLength = this.getCards().length;
+        Deck sortDeck = new Deck(thisDeckLength);
+        if (thisDeckLength == 0 || thisDeckLength == 1) { // if the deck has 0 or 1 cards, return it
+            return this;
+        }
+        // otherwise, divide the deck into two subdecks
+        Deck sub1 = this.subDeck(0, thisDeckLength/2);
+        Deck sub2 = this.subDeck(thisDeckLength/2 + 1, thisDeckLength -1);
+        sub1.mergeSort(); // sort the subdecks using mergeSort
+        sub2.mergeSort();
+        sortDeck = merge(sub1, sub2); // merge the subdecks
+
+        return sortDeck; // return the result
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,50 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 
+    private static Deck merge(Deck d1, Deck d2) {
+        // create a new deck, d3, big enough for all the cards
+        int d1Length = d1.getCards().length;
+        int d2Length = d2.getCards().length;
+        int totalCards = d1Length + d2Length;
+        Deck d3 = new Deck(totalCards);
+        // use the index i to keep track of where we are at in
+        // the first deck, and the index j for the second deck
 
+        int i = 0;
+        int j = 0;
+        // the index k traverses the result deck
+        for (int k = 0; k < totalCards; k++) {
+            // if d1 is empty, use top card from d2
+            // if d2 is empty, use top card from d1
+            // otherwise, compare the top two cards
+            // add lowest card to the new deck at k
+            // increment i or j (depending on card)
+            if (i >= d1Length) {
+                Card nextCard = d2.getCards()[j];
+                d3.setCards(nextCard, k);
+                j++;
+            } else if (j >= d2Length) {
+                Card nextCard = d1.getCards()[i];
+                d3.setCards(nextCard, k);
+                i++;
+            } else {
+                if (d1.getCards()[i].compareTo(d2.getCards()[j]) < 0) {
+                    Card nextCard = d1.getCards()[i];
+                    d3.setCards(nextCard, k);
+                    i++;
+                } else {
+                    Card nextCard = d2.getCards()[j];
+                    d3.setCards(nextCard, k);
+                    j++;
+                }
+            }
+
+
+        }
+        return d3; // return the new deck
+    }
 
 
 
@@ -50,10 +92,59 @@ public class Main {
 
         System.out.println("\n========================= testing " +
                 " binarySearch() ==========================");
-        System.out.println(binarySearch(cards.getCards(), new Card(2,0)));;
+        System.out.println(binarySearch(cards.getCards(), new Card(2,0)));
+
+
+        System.out.println("\n=============== printing deck " +
+                "after shuffle() ==================");
+        cards.shuffle();
+        cards.print();
+
+
+        System.out.println("\n=============== printing deck " +
+                "after selectionSort() ==================");
+        cards.selectionSort();
+        cards.print();
+
+
+        Deck myDeck = new Deck();
+        myDeck.shuffle();
+
+        Deck d1 = myDeck.subDeck(40,51);
+        Deck d2 = myDeck.subDeck(10,20);
+        System.out.println("=".repeat(30) + " deck 1" + "=".repeat(30));
+        d1.print();
+
+        System.out.println("=".repeat(30) + " deck 2" + "=".repeat(30));
+        d2.print();
+
+
+        System.out.println("=".repeat(30) + " deck 1 after sort" + "=".repeat(30));
+        d1.selectionSort();
+        d1.print();
+        System.out.println("=".repeat(30) + " deck 2 after sort" + "=".repeat(30));
+        d2.selectionSort();
+        d2.print();
 
 
 
 
+
+        System.out.println("=".repeat(30) + " merged d3" + "=".repeat(30));
+        Deck d3 = merge(d1,d2);
+        d3.print();
+
+
+
+
+        System.out.println("=".repeat(30) + " sort myDeck with almostMergeSort() " + "=".repeat(30));
+        myDeck = myDeck.almostMergeSort();
+        myDeck.print();
+
+
+        System.out.println("=".repeat(30) + " sort myDeck with mergeSort() " + "=".repeat(30));
+        myDeck.shuffle();
+        myDeck = myDeck.mergeSort();
+        myDeck.print();
     }
 }
